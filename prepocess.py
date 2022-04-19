33858f80c2a11cfa3b0f418cc24b20fa75842e16
HaoyuScut <904264204@qq.com>
2022-04-19 22:23:52 +0800

    second upgrade

--- prepocess.py
+++ prepocess.py
@@ -1,37 +1,37 @@
-import os
-import random
-
-
-trainval_percent = 0.9
-train_percent = 0.9
-xmlfilepath = 'D:\\Dataset\\PCB_dataset\\data\\Annotations'
-txtsavepath = 'D:\\Dataset\\PCB_dataset\\data\\ImageSets'
-total_xml = os.listdir(xmlfilepath)
-
-num = len(total_xml)
-list = range(num)
-tv = int(num * trainval_percent)
-tr = int(tv * train_percent)
-trainval = random.sample(list, tv)
-train = random.sample(trainval, tr)
-
-ftrainval = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\trainval.txt', 'w')
-ftest = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\test.txt', 'w')
-ftrain = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\train.txt', 'w')
-fval = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\val.txt', 'w')
-
-for i in list:
-    name = total_xml[i][:-4] + '\n'
-    if i in trainval:
-        ftrainval.write(name)
-        if i in train:
-            ftrain.write(name)
-        else:
-            fval.write(name)
-    else:
-        ftest.write(name)
-
-ftrainval.close()
-ftrain.close()
-fval.close()
-ftest.close()
+# import os
+# import random
+#
+#
+# trainval_percent = 0.9
+# train_percent = 0.9
+# xmlfilepath = 'D:\\Dataset\\PCB_dataset\\data\\Annotations'
+# txtsavepath = 'D:\\Dataset\\PCB_dataset\\data\\ImageSets'
+# total_xml = os.listdir(xmlfilepath)
+#
+# num = len(total_xml)
+# list = range(num)
+# tv = int(num * trainval_percent)
+# tr = int(tv * train_percent)
+# trainval = random.sample(list, tv)
+# train = random.sample(trainval, tr)
+#
+# ftrainval = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\trainval.txt', 'w')
+# ftest = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\test.txt', 'w')
+# ftrain = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\train.txt', 'w')
+# fval = open('D:\\Dataset\\PCB_dataset\\data\\ImageSets\\val.txt', 'w')
+#
+# for i in list:
+#     name = total_xml[i][:-4] + '\n'
+#     if i in trainval:
+#         ftrainval.write(name)
+#         if i in train:
+#             ftrain.write(name)
+#         else:
+#             fval.write(name)
+#     else:
+#         ftest.write(name)
+#
+# ftrainval.close()
+# ftrain.close()
+# fval.close()
+# ftest.close()
